05a658e0591242881e10b5f288cac129badfc224
Vamshi Prasad <vamvijkum@gmail.com>
2021-05-17 21:55:17 +0530

    Update Navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,29 +1,31 @@
 import React, { Component } from 'react'
 import farmer from '../farmer.png'
+import {Nav} from 'react-bootstrap'
 
 class Navbar extends Component {
 
   render() {
     return (
-      <nav className="navbar navbar-dark fixed-top bg-dark flex-md-nowrap p-0 shadow">
-        <a
-          className="navbar-brand col-sm-3 col-md-2 mr-0"
-          href="http://www.dappuniversity.com/bootcamp"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <img src={farmer} width="30" height="30" className="d-inline-block align-top" alt="" />
-          &nbsp; DApp Token Farm
-        </a>
-
-        <ul className="navbar-nav px-3">
+      <Nav
+  activeKey="/home"
+  /* onSelect={(selectedKey) => alert(`selected ${selectedKey}`)} */
+>
+  <Nav.Item>
+    <Nav.Link href="/">MediScan</Nav.Link>
+  </Nav.Item>
+  <Nav.Item>
+    <Nav.Link eventKey="disabled" disabled>
+    {/* <ul className="navbar-nav px-3">
           <li className="nav-item text-nowrap d-none d-sm-none d-sm-block">
             <small className="text-secondary">
-              <small id="account">{this.props.account}</small>
             </small>
           </li>
-        </ul>
-      </nav>
+        </ul> */}
+        
+        <small id="account">{this.props.account}</small>
+    </Nav.Link>
+  </Nav.Item>
+</Nav>
     );
   }
 }
