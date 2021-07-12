b023fad66f2be62186ba18a2ec21779bce71177f
Vamshi Prasad <vamvijkum@gmail.com>
2021-07-12 12:00:24 +0530

    Upload Images

--- src/server/server.js
+++ src/server/server.js
@@ -1,17 +1,22 @@
 const express = require('express');
-const bodyParser = require('body-parser');
 
 const app = express();
 const port = process.env.PORT || 3000;
 const router = require('./router');
+const path = require('path');
 
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
 
 app.use(express.static('public'));
 app.set('view engine', 'ejs');
 
+app.use('/images/', express.static('/public/images')); 
 app.use('/upload', router);
+
+app.get("/public/images/:id",(req,res)=>{
+  res.sendFile(path.join(__dirname, "./public/images/"+req.params.id))
+})
 
 app.listen(port, function () {
   console.log('Server is running on PORT',port);
